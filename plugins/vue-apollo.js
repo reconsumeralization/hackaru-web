72be3bcf4658f6d479a9cd81f7a41b6952c11424
ktmouk <ktmouk@gmail.com>
2020-06-03 20:52:12 +0900

    apollo-boostを削除

--- plugins/vue-apollo.js
+++ plugins/vue-apollo.js
@@ -1,14 +1,21 @@
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export default ({ app, isServer }) => {
   Vue.use(VueApollo);
 
+  const httpLink = createHttpLink({
+    uri: `${app.$env.HACKARU_API_URL}/graphql`,
+  });
+
   app.apolloProvider = new VueApollo({
     defaultClient: new ApolloClient({
-      uri: `${app.$env.HACKARU_API_URL}/graphql`,
+      link: httpLink,
       ssrMode: isServer,
+      cache: new InMemoryCache(),
     }),
   });
 };
